d5ff79b686b6f9f9d6986dc41bb4386aa5183d87
Albert Hilazo <albhilazo@gmail.com>
2016-06-20 01:55:22 +0200

    Product sheet data retrieved from ElasticSearch

--- src/AppBundle/Controller/ProductController.php
+++ src/AppBundle/Controller/ProductController.php
@@ -9,30 +9,11 @@ use Symfony\Component\HttpFoundation\Request;
 class ProductController extends Controller
 {
     /**
-     * @Route("/product", name="product")
+     * @Route("/product/{productId}", name="product")
      */
-    public function indexAction(Request $request)
+    public function indexAction($productId, Request $request)
     {
-        $productData = [
-          "name"            => "Terras Gauda Etiqueta Negra",
-          "maker"           => "Bodegas Terras Gauda",
-          "concrete_region" => "Galicia",
-          "attributes" => [
-            "vintage"        => [ "value" => "2014" ],
-            "wine_type"      => [ "value" => "blanco" ],
-            "bottle_volume"  => [ "value" => "750" ],
-            "alcohol_volume" => [ "value" => "12.5" ],
-            "grapes" => [
-              [ "value" => "Albariño" ],
-              [ "value" => "Caiño Blanco" ],
-              [ "value" => "Loureiro" ],
-            ],
-          ],
-          "rank" => "4.00",
-          "image_full"           => "https://media-verticommnetwork1.netdna-ssl.com/wines/terras-gauda-etiqueta-negra-286807.jpg",
-          "long_name"            => "Terras Gauda Etiqueta Negra 2014",
-          "producer_description" => "NOTA DE CATA DE <strong>Terras Gauda Etiqueta Negra 2014</strong>: \n- Vista: amarillo paja, intenso, muy atractivo.\n- Nariz: nítidos aromas de clementina, melón, membrillo y piña madura apoyados en delicadas notas de crema catalana, mantequilla fundida y suaves recuerdos de pan tostado.\n- Boca: potente, elegante y madura, amplio, denso y con una buena acidez, augurio de uina gran longevidad. largo final de boca con sensaciones maduras y envolventes,  toques tostados de la madera.\n\nDENOMINACIÓN DE ORIGEN: Rías Baixas.\nVIÑEDO: Bodegas Terras Gauda\nUVAS: 70% Albariño, 20% Loureiro, 10% Caiño blanco. \n\nMARIDAJE DEL VINO: pescados y mariscos. \nTEMPERATURA DE CONSUMO: 12ºC\nGRADUACIÓN ALCOHÓLICA: 12%",
-        ];
+        $productData = $this->get('retrieve_product')->get($productId);
 
         // replace this example code with whatever you need
         return $this->render('default/product.html.twig', [
